82e8acb1b4f6e6019261249a0b242270b51427b1
Mahadi Hasan <mahadih137@gmail.com>
2023-12-22 18:47:12 +0600

    live link added

--- index.js
+++ index.js
@@ -11,7 +11,13 @@ const port = process.env.PORT || 5000;
 // middlewares
 app.use(
   cors({
-    origin: ["http://localhost:5173", "*"],
+    origin: [
+      "https://tasker-job.web.app",
+      "https://tasker-job.surge.sh",
+      "http://tasker-job.surge.sh",
+      "http://localhost:5173",
+      "*",
+    ],
     credentials: true,
     optionsSuccessStatus: 200,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
